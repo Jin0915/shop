a880e14a745dfda8bbdaf5c77529295b99c81a7f
jinjinghui <2281839222@qq.com>
2020-04-02 11:10:10 +0800

    测试2

--- src/store/index.js
+++ src/store/index.js
@@ -6,12 +6,12 @@ Vue.use(Vuex)
 import createPersistedState from 'vuex-persistedstate'
 
 import menu from './modules/menu/index' // index 可以省略 nodejs会进行找
-import admin from './modules/admin/index' // index 可以省略 nodejs会进行找 
+import admin from './modules/admin/index' // index 可以省略 nodejs会进行找
 import card from './modules/card/index' // index 可以省略 nodejs会进行找
 
 export default new Vuex.Store({ // 声明的参数  
 	state: {},
-	mutations:  {},
+	mutations: {},
 	actions: {}, // 可以拆分
 	getters: {},
 	modules: {
@@ -44,7 +44,7 @@ export default new Vuex.Store({ // 声明的参数
 
 // })
 // vuex笔记 
-// 整个应用就是一个state state简单的应用可以使用 复杂了就没有办法维护  解决方案： 把每一个state拆分出去（企业级的中大型下项目）Module--模块化
+// 整个应用就是一个state  state简单的应用可以使用 复杂了就没有办法维护  解决方案： 把每一个state拆分出去（企业级的中大型下项目）Module--模块化
 // 组件依赖的状态放到仓库中,解决非父子组件消息传递问题  
 // 应用场景：非父子组件消息传递或者同步的时候
 // metheds 不区分同步异步 actions 只能异步 mutations 会影响state     computed 类比于getter
